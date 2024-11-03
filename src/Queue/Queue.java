852f583017b428d389ecfe5a24b94beb78229d6a
mohammad-mursalin <mursalinlamon@gmail.com>
2024-11-03 22:48:35 +0600

    queue implementation

--- src/Queue/Queue.java
+++ src/Queue/Queue.java
@@ -7,15 +7,44 @@ public class Queue {
     int rear;
 
     public void enqueue(int data) {
-        queue[rear] = data;
-        rear++;
-        size++;
+        if(isFull()) {
+            System.out.println("The queue is full");
+        }
+        else {
+            queue[rear] = data;
+            rear = (rear+1) % 5;
+            size++;
+        }
+    }
+
+    public void dequeue() {
+        if(isEmpty()) {
+            System.out.println("The queue is empty");
+        }
+        else {
+            System.out.println(queue[front]);
+            front = (front + 1) % 5;
+            size--;
+        }
+    }
+
+    public void size() {
+        System.out.println("Size of the queue : " + (size));
+    }
+
+    public boolean isEmpty() {
+        return (size == 0);
+    }
+
+    public boolean isFull() {
+        return size == 5;
     }
 
     public void show() {
         System.out.print("Elements in the queue : ");
-        for(int i = front ; i < size ; i++) {
-            System.out.print(queue[i] + " ");
+        for(int i = 0 ; i < size ; i++) {
+            System.out.print(queue[(i+front)%5] + " ");
         }
+        System.out.println();
     }
 }
--- src/Queue/Test.java
+++ src/Queue/Test.java
@@ -6,10 +6,21 @@ public class Test {
         Queue queue = new Queue();
 
         queue.enqueue(4);
-        queue.enqueue(4);
-        queue.enqueue(4);
-        queue.enqueue(4);
-
+        queue.enqueue(5);
+        queue.enqueue(6);
+        queue.enqueue(7);
+        queue.enqueue(8);
+        queue.enqueue(9);
+        queue.enqueue(1);
+        queue.size();
         queue.show();
+        queue.dequeue();
+        queue.dequeue();
+        queue.dequeue();
+        queue.dequeue();
+        queue.dequeue();
+        queue.dequeue();
+        queue.show();
+        queue.size();
     }
 }
